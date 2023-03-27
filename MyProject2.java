3761a91713bfc9b701fb291f078767f12b5279be
Wanassanan <94218479+AkaKitsune027@users.noreply.github.com>
2023-03-27 14:59:52 +0700

    meow

--- MyProject2.java
+++ MyProject2.java
@@ -1,7 +1,9 @@
 package myProject2;
 
  class myProject2 {
+ 
      public static void main(String[] args) {
          System.out.println("Hello world");
+         System.out.println("Hello java");
     }         
 }
